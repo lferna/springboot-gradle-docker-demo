a8b138045874dbe6c63d2c1949e12aa2169507db
Luis <lfernapalacio@gmail.com>
2020-06-01 09:40:32 +0200

    Primer commit

---
+++ src/main/java/com/docker/demodocker/DemoDockerApplication.java
@@ -0,0 +1,13 @@
+package com.docker.demodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/docker/demodocker/controller/DemoDockerController.java
@@ -0,0 +1,17 @@
+package com.docker.demodocker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoDockerController {
+
+	
+	@RequestMapping(value={"/hello"}, method = RequestMethod.GET)
+	public String login(){
+		
+		return "Hola Mundo!";
+	}
+	
+}
